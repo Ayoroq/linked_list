b0ac53055cb3e15c25a53b215727459eaa71f50c
Ayo <Ayoroq@gmail.com>
2025-09-04 11:04:30 -0400

    Added more test options to the module file

--- module.js
+++ module.js
@@ -10,13 +10,13 @@ list.append("hamster");
 list.append("snake");
 list.append("turtle");
 
-// console.log(list.toString());
-// console.log(list.size());
-// console.log(list.contains("dog"));
-// console.log(list.contains("monkey"));
-// console.log(list.find("dog"));
-// console.log(list.find("monkey"));
-// list.pop();
-// console.log(list.toString());
-// console.log(list.size());
+console.log(list.toString());
+console.log(list.size());
+console.log(list.contains("dog"));
+console.log(list.contains("monkey"));
+console.log(list.find("dog"));
+console.log(list.find("monkey"));
+list.pop();
+console.log(list.toString());
+console.log(list.size());
 console.log(list.at(100))
